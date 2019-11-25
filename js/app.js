9d7297c7d779f9ccb0bfc969b07a352ed18d317c
Shadia-Nasman <shadia.nasman1988@gmail.com>
2019-11-25 12:55:05 +0100

    first step

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@
 
 
 (() => {
+   
 
     const apiKey= "5e6714ab";
 //////////////////////the eventlistener//////////////////////////////
@@ -14,11 +15,16 @@
     document.querySelector("#run").addEventListener("click", async () => {
 
         const movieName=document.getElementById("nameSearch").value;
-        const movieType=document.getElementById("typeSearch").value;
+        // const movieType=document.getElementById("typeSearch").value;
+
+    /////////////////////////
+    var typ = document.getElementById("typeSearch");
+    const movieType = typ.options[typ.selectedIndex].value;
+    ////////////////////////////
        //fetch
         const response = await fetch("http://www.omdbapi.com/?s="+movieName+"&type="+movieType+"&apikey="+apiKey);
         var result = await response.json();
-        console.log(result);
+        //console.log(result);
         
         const target = document.querySelector("#target");
         
@@ -30,30 +36,22 @@
              years.sort(sortByYear);
             
         });
-
         console.log(years);
-            years.forEach(element => {
-            
-          
-
-            ///////////////creating new elements to display the results/////////////
 
 
-const displayMovie = document.querySelector(".displayContainer");
+ years.forEach(element => {
+     ///////////////creating new elements to display the results/////////////
+
+const displayMovie = document.querySelector(".displayElements");
     const newElement = displayMovie.cloneNode(true);
+    target.appendChild(newElement);
 
-    newElement.querySelector(".movie-Type").innerHTML =element.Type;
-   
-    target.appendChild(newElement)
-    newElement.querySelector(".movie-Year").innerHTML =element.Year;
-    newElement.querySelector(".movie-Title").innerHTML =element.Title;
-   
-    show_image(element.Poster,200,200,'logo');
-
-
+    newElement.querySelector(".movie-Type").innerHTML ="Type: "+element.Type;  
+    newElement.querySelector(".movie-Year").innerHTML ="Year: "+element.Year.slice(0,4);
+    newElement.querySelector(".movie-Title").innerHTML ="Title:<br> "+element.Title;
+    show_image(element.Poster,200,300,'logo');
     });
-  
-   
+
     });
 
 ///////////////////////function to display the foto///////////
@@ -69,7 +67,9 @@ const displayMovie = document.querySelector(".displayContainer");
     }
     
 const sortByYear = (a, b) => {
-  return b.Year - a.Year;
+    var A=a.Year.slice(0,4);
+    var B=b.Year.slice(0,4);
+  return B-A;
 };
 
 
